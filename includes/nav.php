56a028f80612f3e4663e229c3ef6449433756bc4
felipeporte <felipe.porte@pesaschile.cl>
2025-06-19 15:04:43 -0400

    se modifica nav y agrega pagina deportista

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <!-- includes/nav.php -->
-
+<?php $actual = basename($_SERVER['PHP_SELF']); ?>
 <nav class="main-menu">
   <h1>Mai App</h1>
   <img class="logo" src="/0mai/maiteam/asset/img/asset1.png" alt="" />
@@ -8,7 +8,7 @@
       <b></b><b></b>
       <a href="./../dashboard.php"><i class="fa fa-house nav-icon"></i><span class="nav-text">Home</span></a>
     </li>
-    <li class="nav-item <?= $actual === 'perfil.php' ? 'active' : '' ?>">
+    <li class="nav-item <?= $actual === 'finanzas.php' ? 'active' : '' ?>">
       <b></b><b></b>
       <a href="nav/finanzas.php"><i class="fa fa-hand-holding-usd nav-icon"></i><span class="nav-text">Finanzas</span></a>
     </li>
@@ -16,9 +16,9 @@
       <b></b><b></b>
       <a href="calendario.php"><i class="fa fa-calendar-check nav-icon"></i><span class="nav-text">Calendario</span></a>
     </li>
-    <li class="nav-item <?= $actual === 'actividades.php' ? 'active' : '' ?>">
+    <li class="nav-item <?= $actual === 'deportista.php' ? 'active' : '' ?>">
       <b></b><b></b>
-      <a href="actividades.php"><i class="fa fa-person-running nav-icon"></i><span class="nav-text">Deportista</span></a>
+      <a href="deportista.php"><i class="fa fa-person-running nav-icon"></i><span class="nav-text">Deportista</span></a>
     </li>
     <li class="nav-item <?= $actual === 'ajustes.php' ? 'active' : '' ?>">
       <b></b><b></b>
---
+++ nav/deportista.php
@@ -0,0 +1,85 @@
+<?php
+include './../includes/header.php';
+include './../includes/nav.php';
+?>
+
+
+  <section class="content">
+        <div class="left-content">
+            <div class="activities">
+                <h1>Tabla de ejemplo</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="right-content">
+          <div class="user-info">
+            <div class="icon-container">
+              <i class="fa fa-bell nav-icon"></i>
+              <i class="fa fa-message nav-icon"></i>
+            </div>
+            <h4>Maria Flores</h4>
+            <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/40b7cce2-c289-4954-9be0-938479832a9c" alt="user" />
+          </div>
+
+          
+          
+        </div>
+    
+           
+
+</section>
+
+<?php include './../includes/footer.php'; ?>
